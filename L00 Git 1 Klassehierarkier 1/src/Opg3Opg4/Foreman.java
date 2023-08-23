f44cf2d4341f4c7639d13e89939102931f4f3232
PatrickTyme <Patricktyme@hotmail.com>
2023-08-23 20:41:50 +0200

    Done

--- L00 Git 1 Klassehierarkier 1/src/Opg3Opg4/Foreman.java
+++ L00 Git 1 Klassehierarkier 1/src/Opg3Opg4/Foreman.java
@@ -1,22 +1,28 @@
 package Opg3Opg4;
 
-import java.util.ArrayList;
 
 public class Foreman extends Mechanic {
-    private String startForeman;
+    private int promotionYear;
     private int weeklyAddedMoney;
 
-    public String getStartForeman() {
-        return startForeman;
+    public Foreman(String name, String address, int apprenticeYear, int hourlyWage, int promotionYear, int weeklyAddedMoney) {
+        super(name, address, apprenticeYear, hourlyWage);
+        this.promotionYear = promotionYear;
+        this.weeklyAddedMoney = weeklyAddedMoney;
     }
 
-    public int getWeeklyAddedMoney() {
-        return weeklyAddedMoney;
-    }
 
-    @Override
-    public int getHourlyWage() {
-        return super.getHourlyWage();
-    }
+        public int getWeeklyAddedMoney () {
+            return this.weeklyAddedMoney;
+        }
 
+        @Override
+        public int getHourlyWage () {
+            return super.getHourlyWage();
+        }
+
+
+    public int getPromotionYear() {
+        return promotionYear;
+    }
 }
--- L00 Git 1 Klassehierarkier 1/src/Opg3Opg4/Mechanic.java
+++ L00 Git 1 Klassehierarkier 1/src/Opg3Opg4/Mechanic.java
@@ -1,14 +1,28 @@
 package Opg3Opg4;
 
 public class Mechanic extends Person {
-    private String graduation;
+    private int apprenticeYear;
     private int hourlyWage;
 
-    public String getGraduation() {
-        return graduation;
+    public Mechanic(String name, String address, int apprenticeYear, int hourlyWage) {
+        super(name, address);
+        this.apprenticeYear = apprenticeYear;
+        this.hourlyWage = hourlyWage;
+
+    }
+    // Opg4 a)
+    public int weeklySalary() {
+        int weeklyHours = 37;
+        return hourlyWage * weeklyHours;
     }
 
-    public int getHourlyWage() {
-        return hourlyWage;
+        public int getHourlyWage() {
+            return hourlyWage;
+        }
+
+
+    public int getApprenticeYear() {
+        return apprenticeYear;
     }
+
 }
