b2d80da59927cd718e21cb6cc241ac6f2ec25e0b
PatrickTyme <Patricktyme@hotmail.com>
2023-08-24 08:24:59 +0200

    Update

--- L00 Git 1 Klassehierarkier 1/src/Opg3Opg4/Test.java
+++ L00 Git 1 Klassehierarkier 1/src/Opg3Opg4/Test.java
@@ -7,13 +7,14 @@ public class Test {
         Person person = new Person("Jan Bo", "Hulbo 48");
 
         ArrayList<Mechanic> mechanicList = new ArrayList<>();
+        //Mechanic
         mechanicList.add(new Mechanic("Ludvig", "Göteborg Allé 15", 2007, 125));
         mechanicList.add(new Mechanic("Karen-Hanne", "Nejtak 28", 2018, 115));
         mechanicList.add(new Mechanic("John Arne", "Husvilla 82", 2001, 128));
-
+        //Foreman
         mechanicList.add(new Foreman("Forsen", "Sjusk 53", 1988, 130, 2012, 1000));
         mechanicList.add(new Foreman("Ole Ib", "Sjask 19", 2015, 125, 2020, 1000));
-
+        //Surveyor
         mechanicList.add(new Surveyor("Suresen", "Bi Allé 72", 1999, 145, 30));
 
         int totalSalary = totalWeeklySalary(mechanicList);
